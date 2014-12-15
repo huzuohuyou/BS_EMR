a9b8840cdcb35727e74523afbfc98f2fad38c482
huzuohuyou <1255246777@qq.com>
2014-12-15 09:55:00 +0800

    second commit
    second commit

---
+++ MvcApplication2014-12-08/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcApplication2014_12_08.MvcApplication" Language="C#" %>
---
+++ MvcApplication2014-12-08/Scripts/datagrid-groupview.js
@@ -0,0 +1,22 @@
+﻿$('#tt').datagrid({
+    title: 'DataGrid - GroupView',
+    width: 500,
+    height: 250,
+    rownumbers: true,
+    remoteSort: false,
+    nowrap: false,
+    fitColumns: true,
+    url: 'datagrid_data.json',
+    columns: [[
+			{ field: 'productid', title: 'Product ID', width: 100, sortable: true },
+			{ field: 'listprice', title: 'List Price', width: 80, align: 'right', sortable: true },
+			{ field: 'unitcost', title: 'Unit Cost', width: 80, align: 'right', sortable: true },
+			{ field: 'attr1', title: 'Attribute', width: 150, sortable: true },
+			{ field: 'status', title: 'Status', width: 60, align: 'center' }
+		]],
+    groupField: 'productid',
+    view: groupview,
+    groupFormatter: function (value, rows) {
+        return value + ' - ' + rows.length + ' Item(s)';
+    }
+});
